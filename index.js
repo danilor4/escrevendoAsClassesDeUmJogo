721472eb22cf8172c5e6c97e31ed0d49b2531975
danilor4 <danilolima180@gmail.com>
2025-07-19 13:24:44 -0300

    criação das variaveis

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 class heroi {
-    constructor(){
-        
+    constructor(nome, idade, tipo){
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
     }
+    atacar() {
+        con
+    }
+
+    let mago = new heroi("Harry",'55',"mago");
+    let guerreiro = new heroi("Connan",'30',"guerreiro");
+    let ninja = new heroi("Naruto","18","ninja");
+    let assassino = new heroi("zoro","20","assassino");
 }
