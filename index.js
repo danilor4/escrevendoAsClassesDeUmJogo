44f5ad32999ac0806dbe596e79249ca20585c9e3
danilor4 <danilolima180@gmail.com>
2025-07-19 13:45:47 -0300

    Finalizado

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 class heroi {
-    constructor(nome, idade, tipo){
+    constructor(nome, idade, tipo, ataque) {
         this.nome = nome;
         this.idade = idade;
         this.tipo = tipo;
+        this.ataque = ataque; // Corrigido!
     }
     atacar() {
-        con
+        console.log(`O ${this.tipo} ${this.nome} ataca com sua golpe! ${this.ataque}`);
     }
-
-    let mago = new heroi("Harry",'55',"mago");
-    let guerreiro = new heroi("Connan",'30',"guerreiro");
-    let ninja = new heroi("Naruto","18","ninja");
-    let assassino = new heroi("zoro","20","assassino");
 }
+
+let mago = new heroi("Harry", 55, "mago", "magia");
+let guerreiro = new heroi("Connan", 30, "guerreiro", "espadada");
+let ninja = new heroi("Naruto", 18, "ninja", "rasengan");
+let assassino = new heroi("zoro", 20, "assassino", "facada na guela");
+
+mago.atacar();
+guerreiro.atacar();
+ninja.atacar();
+assassino.atacar();
